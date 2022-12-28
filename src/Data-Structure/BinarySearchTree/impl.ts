1cadad7a311ec9d8632b498a4014d27c8c38b5a7
jaylen <jaylen.work@hotmail.com>
2022-12-28 11:04:01 +0800

    feat(data-structure): BinarySearchTree remove API

--- src/Data-Structure/BinarySearchTree/impl.ts
+++ src/Data-Structure/BinarySearchTree/impl.ts
@@ -51,8 +51,127 @@ class BinarySearchTree<E> implements IBinarySearchTree<E> {
     return root;
   }
 
+  // 寻找二分搜索树当中的最小元素
+  minimum(): E {
+    if (this.size == 0) {
+      throw new Error('BinarySearchTree is empty!');
+    }
+
+    return this._minimum(this.root!).e;
+  }
+
+  private _minimum(root: TNode<E>): TNode<E> {
+    if (root.left == null) return root;
+
+    return this._minimum(root.left);
+  }
+
+  // 寻找二分搜索树当中的最大元素
+  maxmum(): E {
+    if (this.size == 0) {
+      throw new Error('BinarySearchTree is empty!');
+    }
+
+    return this._maxmum(this.root!).e;
+  }
+
+  private _maxmum(root: TNode<E>): TNode<E> {
+    if (root.right == null) return root;
+
+    return this._maxmum(root.right);
+  }
+
+  // 从二分搜索树当中删除最小的节点，并返回最小值
+  // 删除掉以root为根的二分搜索树当中的最小节点
+  removeMin(): E {
+    if (this.root == null) throw new Error('BinarySearchTree is empty!');
+
+    const result = this.minimum();
+    this.root = this._removeMin(this.root);
+
+    return result;
+  }
+
+  // 删除最小值有两种场景
+  // 场景1：删除的元素是整个树中的叶子节点 - 直接删除即可
+  // 场景2：删除的元素不是整个树中的叶子节点 - 将该元素的右子树保留，删除该元素，然后让保留的右子树直接替代这个元素成为父亲元素的左子树
+  private _removeMin(root: TNode<E>): TNode<E> | null {
+    if (root.left == null) {
+      this.size -= 1;
+      const rightNode = root.right;
+      root.right = null;
+      return rightNode;
+    } else {
+      root.left = this._removeMin(root.left);
+      return root;
+    }
+  }
+
+  removeMax(): E {
+    if (this.root == null) throw new Error('BinarySearchTree is empty!');
+
+    const result = this.maxmum();
+    this._removeMax(this.root);
+
+    return result;
+  }
+
+  private _removeMax(root: TNode<E>): TNode<E> | null {
+    if (root.right == null) {
+      this.size -= 1;
+      const leftNode = root.left;
+      root.left = null;
+      return leftNode;
+    } else {
+      root.right = this._removeMax(root.right);
+      return root;
+    }
+  }
+
   remove(e: E) {
-    console.log(e);
+    this.root = this._remove(this.root, e);
+  }
+
+  // 删除以node为根的二分搜索树中指为e的节点
+  // 返回删除节点后新的二分搜索树的根
+  private _remove(root: TNode<E> | null, e: E): TNode<E> | null {
+    if (root == null) {
+      return null;
+    } else {
+      if (e < root.e) {
+        root.left = this._remove(root.left, e);
+
+        return root;
+      } else if (e > root.e) {
+        root.right = this._remove(root.right, e);
+
+        return root;
+      } else {
+        // e == root.e
+
+        if (root.left == null) {
+          const rightNode = root.right;
+          root.right = null;
+          this.size -= 1;
+          return rightNode;
+        }
+        if (root.right == null) {
+          const leftNode = root.left;
+          root.left = null;
+          this.size -= 1;
+          return leftNode;
+        }
+
+        // 待删除的节点左右子树均不为空
+        const successor = this._minimum(root.right);
+        successor.right = this._removeMin(root.right);
+        successor.left = root.left;
+
+        root.left = root.right = null;
+
+        return successor;
+      }
+    }
   }
 
   // 二分搜索树中是否包含e
@@ -78,6 +197,8 @@ class BinarySearchTree<E> implements IBinarySearchTree<E> {
   }
 
   // 以root为根的二分搜索树进行前序遍历
+  // 找到比待删除节点大的最小节点，其实就是待删除节点右子树上的最小节点
+  // 用这个节点顶替待删除节点的位置
   private _preOrder(root: TNode<E> | null) {
     if (root == null) return;
 
