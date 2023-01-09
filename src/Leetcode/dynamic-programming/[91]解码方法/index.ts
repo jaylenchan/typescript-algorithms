dc2cbbe217587efe559ed6f24e7bb3e88846dc7c
jaylen <jaylen.work@hotmail.com>
2023-01-09 16:14:07 +0800

    feat(leetcode): [91]解码方法

--- src/Leetcode/dynamic-programming/[91]解码方法/index.ts
+++ src/Leetcode/dynamic-programming/[91]解码方法/index.ts
@@ -13,14 +13,18 @@ function numDecodings(s: string): number {
   dp[0] = 1;
 
   for (let i = 1; i <= len; i++) {
-    if (s.charAt(i - 1) != '0') dp[i] = dp[i - 1];
+    if (s.charAt(i - 1) != '0') {
+      dp[i] = dp[i - 1];
+    }
+
     //判断截取两个是否符合
     if (
       i >= 2 &&
       (s.charAt(i - 2) == '1' ||
         (s.charAt(i - 2) == '2' && s.charAt(i - 1) <= '6'))
-    )
+    ) {
       dp[i] += dp[i - 2];
+    }
   }
 
   return dp[len];
