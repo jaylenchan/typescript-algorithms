564d5d5f4d836c2825d830aea8b10770c9b10298
jaylen <jaylen.work@hotmail.com>
2023-01-11 14:26:35 +0800

    feat(data-structure): BinarySearchTreeMap impl

--- src/Data-Structure/Map/BinarySearchTreeMap-impl.ts
+++ src/Data-Structure/Map/BinarySearchTreeMap-impl.ts
@@ -1,39 +1,167 @@
-import BinarySearchTree from '../BinarySearchTree/impl';
 import IMap from './interface';
 
+class TNode<K, V> {
+  public key: K;
+  public value: V;
+  public left: TNode<K, V> | null;
+  public right: TNode<K, V> | null;
+
+  constructor(key: K, value: V) {
+    this.key = key;
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTreeMap<K, V> implements IMap<K, V> {
-  private bst: BinarySearchTree<K>;
+  private root: TNode<K, V> | null;
+  private size: number;
 
   constructor() {
-    this.bst = new BinarySearchTree<K>();
+    this.root = null;
+    this.size = 0;
   }
 
-  add(k: K, v: V): void {
-    throw new Error('Method not implemented.');
+  // 向二分搜索树当中添加新的key，value
+  add(key: K, value: V): void {
+    this.root = this._add(this.root, key, value);
   }
 
-  remove(k: K): V {
-    throw new Error('Method not implemented.');
+  // 向以root为根的二分搜索树当中插入key，value
+  // 返回插入新节点后二分搜索树的根
+  private _add(root: TNode<K, V> | null, key: K, value: V): TNode<K, V> {
+    if (root == null) {
+      this.size += 1;
+      return new TNode(key, value);
+    }
+
+    if (root.key < key) {
+      root.left = this._add(root.left, key, value);
+    } else if (root.key > key) {
+      root.right = this._add(root.right, key, value);
+    } else {
+      root.value = value;
+    }
+
+    return root;
   }
 
-  contains(k: K): boolean {
-    throw new Error('Method not implemented.');
+  // 返回以root为根的二分搜索树中，key所在的节点
+  private _getNode(root: TNode<K, V> | null, key: K): TNode<K, V> | null {
+    if (root == null) return null;
+
+    if (root.key == key) {
+      return root;
+    } else if (root.key > key) {
+      return this._getNode(root.right, key);
+    } else {
+      return this._getNode(root.left, key);
+    }
   }
 
-  get(k: K): V {
-    throw new Error('Method not implemented.');
+  // 返回以root为根的二分搜索树中的最小节点
+  private _minimum(root: TNode<K, V>): TNode<K, V> {
+    if (root.left == null) {
+      return root;
+    } else {
+      return this._minimum(root);
+    }
   }
 
-  set(k: K, v: V): void {
-    throw new Error('Method not implemented.');
+  // 删除掉以root为根的二分搜索树当中的最小节点
+  // 返回删除节点后新的二分搜索树的根
+  private _removeMin(root: TNode<K, V>): TNode<K, V> | null {
+    if (root.left == null) {
+      this.size -= 1;
+      const rightNode = root.right;
+      root.right = null;
+      return rightNode;
+    } else {
+      root.left = this._removeMin(root.left);
+      return root;
+    }
+  }
+
+  // 删除以node为根的二分搜索树中键为key的节点
+  // 返回删除节点后新的二分搜索树的根
+  private _remove(root: TNode<K, V> | null, key: K): TNode<K, V> | null {
+    if (root == null) {
+      return null;
+    } else {
+      if (key < root.key) {
+        root.left = this._remove(root.left, key);
+
+        return root;
+      } else if (key > root.key) {
+        root.right = this._remove(root.right, key);
+
+        return root;
+      } else {
+        if (root.left == null) {
+          const rightNode = root.right;
+          root.right = null;
+          this.size -= 1;
+          return rightNode;
+        }
+        if (root.right == null) {
+          const leftNode = root.left;
+          root.left = null;
+          this.size -= 1;
+          return leftNode;
+        }
+
+        // 待删除的节点左右子树均不为空
+        const successor = this._minimum(root.right);
+        successor.right = this._removeMin(root.right);
+        successor.left = root.left;
+
+        root.left = root.right = null;
+
+        return successor;
+      }
+    }
+  }
+
+  remove(key: K): V | null {
+    const node = this._getNode(this.root, key);
+
+    if (node != null) {
+      this.root = this._remove(this.root, key);
+      return node.value;
+    }
+
+    return null;
+  }
+
+  contains(key: K): boolean {
+    const node = this._getNode(this.root, key);
+
+    return node != null;
+  }
+
+  get(key: K): V | null {
+    const node = this._getNode(this.root, key);
+
+    return node == null ? null : node.value;
+  }
+
+  set(key: K, newValue: V): void {
+    const node = this._getNode(this.root, key);
+
+    if (node == null) {
+      throw new Error(`${key} doesn't exist!`);
+    } else {
+      node.value = newValue;
+    }
   }
 
   getSize(): number {
-    throw new Error('Method not implemented.');
+    return this.size;
   }
 
   isEmpty(): boolean {
-    throw new Error('Method not implemented.');
+    return this.size == 0;
   }
 }
 
