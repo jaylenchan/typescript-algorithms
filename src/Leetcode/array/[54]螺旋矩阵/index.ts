323d9054095da8510784a10ee2c31076474404b0
jaylen <jaylen.work@hotmail.com>
2023-01-19 22:26:09 +0800

    feat(leetcode): [54]螺旋矩阵

--- src/Leetcode/array/[54]螺旋矩阵/index.ts
+++ src/Leetcode/array/[54]螺旋矩阵/index.ts
@@ -5,40 +5,69 @@
  */
 
 // @lc code=start
+
 function spiralOrder(matrix: number[][]): number[] {
-  let top = 0;
-  let down = matrix.length - 1;
-  let left = 0;
-  let right = matrix[0].length - 1;
+  const result: number[] = [];
+  let firstRow = 0;
+  let lastRow = matrix.length - 1;
+  let firstCol = 0;
+  let lastCol = matrix[0].length - 1;
 
-  const result = [];
-
-  while (true) {
-    for (let i = left; i <= right; i++) {
-      result.push(matrix[top][i]);
-    }
-    top += 1;
-    if (top > down) break;
-
-    for (let i = top; i <= down; i++) {
-      result.push(matrix[i][right]);
-    }
-    right -= 1;
-    if (right < left) break;
-
-    for (let i = right; i >= left; i--) {
-      result.push(matrix[down][i]);
-    }
-    down -= 1;
-    if (top > down) break;
-
-    for (let i = down; i >= top; i--) {
-      result.push(matrix[i][left]);
-    }
-    left += 1;
-    if (right < left) break;
+  while (firstRow <= lastRow && firstCol <= lastCol) {
+    _spiralOrder(matrix, firstRow, lastRow, firstCol, lastCol, result);
+    firstRow += 1;
+    firstCol += 1;
+    lastRow -= 1;
+    lastCol -= 1;
   }
 
   return result;
 }
+
+function _spiralOrder(
+  matrix: number[][],
+  top: number,
+  down: number,
+  left: number,
+  right: number,
+  result: number[]
+) {
+  const firstRow = top;
+  const firstCol = left;
+  const lastRow = down;
+  const lastCol = right;
+
+  if (top == down) {
+    for (let i = left; i <= right; i++) {
+      result.push(matrix[top][i]);
+    }
+  } else if (left == right) {
+    for (let i = top; i <= down; i++) {
+      result.push(matrix[i][left]);
+    }
+  } else {
+    while (left < lastCol) {
+      result.push(matrix[firstRow][left]);
+      left += 1;
+    }
+
+    while (top < lastRow) {
+      result.push(matrix[top][lastCol]);
+      top += 1;
+    }
+
+    while (right > firstCol) {
+      result.push(matrix[lastRow][right]);
+      right -= 1;
+    }
+
+    while (down > firstRow) {
+      result.push(matrix[down][firstCol]);
+      down -= 1;
+    }
+  }
+}
+
+// 思路：考虑矩阵宏观代码调度，不要陷入局部坐标变换怎么跑的。
+
 // @lc code=end
