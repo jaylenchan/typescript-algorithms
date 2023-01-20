f639a965fce9a4e2dcc4599a38a1f75ea86d81f9
jaylen <jaylen.work@hotmail.com>
2023-01-20 11:58:12 +0800

    feat(leetcode): [73]矩阵置零

---
+++ src/Leetcode/array/[73]矩阵置零/index.ts
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=73 lang=typescript
+ *
+ * [73] 矩阵置零
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix: number[][]): void {
+  let firstRow = 0;
+  let firstCol = 0;
+  let lastRow = matrix.length - 1;
+  let lastCol = matrix[0].length - 1;
+
+  const flag = Math.pow(2, 31);
+
+  for (let row = firstRow; row <= lastRow; row++) {
+    for (let col = firstCol; col <= lastCol; col++) {
+      if (matrix[row][col] == 0) {
+        _setZeroes(matrix, row, col);
+      }
+    }
+  }
+
+  for (let row = firstRow; row <= lastRow; row++) {
+    for (let col = firstCol; col <= lastCol; col++) {
+      if (matrix[row][col] == flag) {
+        matrix[row][col] = 0;
+      }
+    }
+  }
+}
+
+function _setZeroes(matrix: number[][], curRow: number, curCol: number): void {
+  let left = curCol - 1;
+  let right = curCol + 1;
+  let top = curRow - 1;
+  let down = curRow + 1;
+
+  let lastRow = matrix.length - 1;
+  let lastCol = matrix[0].length - 1;
+
+  const flag = Math.pow(2, 31);
+
+  matrix[curRow][curCol] = flag;
+  while (left >= 0 && matrix[curRow][left] != 0) {
+    matrix[curRow][left] = flag;
+    left -= 1;
+  }
+
+  while (right <= lastCol && matrix[curRow][right] != 0) {
+    matrix[curRow][right] = flag;
+    right += 1;
+  }
+
+  while (top >= 0 && matrix[top][curCol] != 0) {
+    matrix[top][curCol] = flag;
+    top -= 1;
+  }
+
+  while (down <= lastRow && matrix[down][curCol] != 0) {
+    matrix[down][curCol] = flag;
+    down += 1;
+  }
+}
+// @lc code=end
