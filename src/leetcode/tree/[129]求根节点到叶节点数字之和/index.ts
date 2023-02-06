79fdc3f811fca8ec8377fad7cd569af256967aea
jaylen <jaylen.work@hotmail.com>
2023-02-06 19:53:58 +0800

    feat(leetcode): [129]求根节点到叶节点数字之和

--- src/leetcode/tree/[129]求根节点到叶节点数字之和/index.ts
+++ src/leetcode/tree/[129]求根节点到叶节点数字之和/index.ts
@@ -22,29 +22,44 @@ export default sumNumbers;
  */
 
 function sumNumbers(root: TreeNode | null): number {
-  const pathList: number[] = [];
+  if (root == null) return 0;
+  return pathSum(root, 0);
+}
+
+/* 返回以node为根节点的树到叶子节点的所有路径转换成数字后的总和 */
+function pathSum(node: TreeNode, preSum: number): number {
+  // 终止节点就是已经到叶子节点了
+  if (!node.left && !node.right) {
+    return preSum * 10 + node.val;
+  }
+
+  preSum = preSum * 10 + node.val;
   let sum = 0;
-
-  traverse(root, '', pathList);
-
-  for (let i = 0; i < pathList.length; i++) {
-    sum += pathList[i];
+  if (node.left) {
+    sum += pathSum(node.left, preSum);
+  }
+  if (node.right) {
+    sum += pathSum(node.right, preSum);
   }
 
   return sum;
 }
 
-function traverse(root: TreeNode | null, path: string, pathList: number[]) {
-  if (root == null) {
-    return;
-  } else {
-    if (root.left == null && root.right == null) {
-      pathList.push(Number(path + root.val));
-      return;
-    }
-
-    traverse(root.left, path + root.val, pathList);
-    traverse(root.right, path + root.val, pathList);
-  }
-}
+/**
+ * 思路：
+ * 定义pathSum函数，语义是返回以node为根的数到叶子节点的路径转化的数字总和
+ * base case是节点是叶子节点了
+ * 如果非base case，则获取利用公式cur * 10 + node.val获取达到当前节点所
+ * 转换的数字的大小
+ * 递归获取左右子树的路径总和，然后相加起来就是根节点的树的路径总和
+ *
+ * 技巧：
+ * 给定字符串"123",求数字的方式就是
+ * 字符串从左往右遍历，依照公式有
+ * 0 * 10 + 1 = 1
+ * 1 * 10 + 2 = 12
+ * 12* 10 + 3 = 123
+ * let sum = 0;
+ * sum = sum*10 + curVal
+ */
 // @lc code=end
