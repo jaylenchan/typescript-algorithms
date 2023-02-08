e0f518222e92ee8bb87c079a51f0019257f35762
jaylen <jaylen.work@hotmail.com>
2023-02-08 14:53:38 +0800

    refactor(leetcode): 修改三数之和解法为通用解

--- src/leetcode/two-pointers/[15]三数之和/index.ts
+++ src/leetcode/two-pointers/[15]三数之和/index.ts
@@ -5,42 +5,65 @@
  */
 
 // @lc code=start
-function threeSum(nums: number[]): number[][] {
-  const result: number[][] = [];
-  nums.sort((a, b) => a - b);
+function twoSum(
+  nums: number[],
+  begin: number,
+  end: number,
+  target: number
+): number[][] {
+  const ans: number[][] = [];
 
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (nums[i] > 0) break;
-    if (i > 0 && nums[i] == nums[i - 1]) continue;
+  let L = begin;
+  let R = end;
 
-    let target = -nums[i];
-    let left = i + 1;
-    let right = nums.length - 1;
-
-    while (left < right) {
-      if (target == nums[left] + nums[right]) {
-        result.push([-target, nums[left], nums[right]]);
-
-        while (nums[left + 1] == nums[left]) {
-          left += 1;
-        }
-
-        while (nums[right - 1] == nums[right]) {
-          right -= 1;
-        }
-
-        left += 1;
-        right -= 1;
-      } else if (target > nums[left] + nums[right]) {
-        // 说明和小了，需要增大和靠近目标,left右移
-        left += 1;
-      } else {
-        // 说明和大了，需要减小和靠近目标,right左移
-        right -= 1;
+  while (L < R) {
+    const numL = nums[L];
+    const numR = nums[R];
+    if (numL + numR == target) {
+      ans.push([-target, numL, numR]);
+      L++;
+      R--;
+      while (nums[L] == numL) {
+        L++;
       }
+
+      while (nums[R] == numR) {
+        R--;
+      }
+    } else if (numL + numR < target) {
+      L++;
+    } else {
+      R--;
     }
   }
 
-  return result;
+  return ans;
+}
+
+function threeSum(nums: number[]): number[][] {
+  const ans: number[][] = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 2; ) {
+    const curRes = twoSum(nums, i + 1, nums.length - 1, -nums[i]);
+    for (let j = 0; j < curRes.length; j++) {
+      ans.push(curRes[j]);
+    }
+
+    i++;
+    while (nums[i] == nums[i - 1]) {
+      i++;
+    }
+  }
+
+  return ans;
 }
 // @lc code=end
+
+/**
+ * #思路#
+ * 1. 三数之和的思路就是枚举一遍数组，将每个数的相反数作为target
+ * 2. 对每一个target进行两数之和操作
+ * 3. 当枚举的值重复的时候，直接跳过，不需要对重复值进行两数之和操作
+ * 4. 遍历完所有target的两数之和操作后，收集返回总结果
+ */
