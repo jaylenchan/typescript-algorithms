e70a01dfd9787670028eebd7cd8efaecd0159713
jaylen <jaylen.work@hotmail.com>
2023-02-08 17:07:54 +0800

    refactor(leetcode): 找到N数之和通解

--- src/leetcode/two-pointers/[15]三数之和/index.ts
+++ src/leetcode/two-pointers/[15]三数之和/index.ts
@@ -6,22 +6,17 @@
 export { threeSum };
 
 // @lc code=start
-function twoSum(
-  nums: number[],
-  begin: number,
-  end: number,
-  target: number
-): number[][] {
+function twoSum(nums: number[], begin: number, target: number): number[][] {
   const ans: number[][] = [];
 
   let L = begin;
-  let R = end;
+  let R = nums.length - 1;
 
   while (L < R) {
     const numL = nums[L];
     const numR = nums[R];
     if (numL + numR == target) {
-      ans.push([-target, numL, numR]);
+      ans.push([numL, numR]);
       L++;
       R--;
       while (nums[L] == numL) {
@@ -41,23 +36,33 @@ function twoSum(
   return ans;
 }
 
-function threeSum(nums: number[]): number[][] {
-  const ans: number[][] = [];
-  nums.sort((a, b) => a - b);
+function getSum(
+  nums: number[],
+  begin: number,
+  target: number,
+  level: number
+): number[][] {
+  if (level == 1) return twoSum(nums, begin, target);
 
-  for (let i = 0; i < nums.length - 2; ) {
-    const curRes = twoSum(nums, i + 1, nums.length - 1, -nums[i]);
+  const ans: number[][] = [];
+  for (let i = begin; i < nums.length - level; ) {
+    const curRes = getSum(nums, i + 1, target - nums[i], level - 1);
+
     for (let j = 0; j < curRes.length; j++) {
-      ans.push(curRes[j]);
+      ans.push([nums[i], ...curRes[j]]);
     }
 
     i++;
-    while (nums[i] == nums[i - 1]) {
-      i++;
-    }
+    while (nums[i] == nums[i - 1]) i++;
   }
 
   return ans;
+}
+
+function threeSum(nums: number[]): number[][] {
+  nums.sort((a, b) => a - b);
+
+  return getSum(nums, 0, 0, 2);
 }
 // @lc code=end
 
@@ -67,4 +72,7 @@ function threeSum(nums: number[]): number[][] {
  * 2. 对每一个target进行两数之和操作
  * 3. 当枚举的值重复的时候，直接跳过，不需要对重复值进行两数之和操作
  * 4. 遍历完所有target的两数之和操作后，收集返回总结果
+ *
+ * #通解#
+ * 经过四数之和，我找到了n数之和的通解，写成了递归函数getSum。使用前对数组进行排序，调用getSum即可得到n数之和
  */
--- src/leetcode/two-pointers/[18]四数之和/index.ts
+++ src/leetcode/two-pointers/[18]四数之和/index.ts
@@ -5,16 +5,12 @@
  */
 export { fourSum };
 // @lc code=start
-function twoSum(
-  nums: number[],
-  begin: number,
-  end: number,
-  target: number
-): number[][] {
+
+function twoSum(nums: number[], begin: number, target: number): number[][] {
   const ans2: number[][] = [];
 
   let L = begin;
-  let R = end;
+  let R = nums.length - 1;
 
   while (L < R) {
     const numL = nums[L];
@@ -46,37 +42,33 @@ function twoSum(
   return ans2;
 }
 
-function treeSum(nums: number[], begin: number, target: number): number[][] {
-  const ans3: number[][] = [];
-  for (let i = begin; i < nums.length - 2; ) {
-    const curRes = twoSum(nums, i + 1, nums.length - 1, target - nums[i]);
+function getSum(
+  nums: number[],
+  begin: number,
+  target: number,
+  level: number
+): number[][] {
+  if (level == 1) return twoSum(nums, begin, target);
+
+  const ans: number[][] = [];
+  for (let i = begin; i < nums.length - level; ) {
+    const curRes = getSum(nums, i + 1, target - nums[i], level - 1);
 
     for (let j = 0; j < curRes.length; j++) {
-      ans3.push([nums[i], ...curRes[j]]);
+      ans.push([nums[i], ...curRes[j]]);
     }
+
     i++;
     while (nums[i] == nums[i - 1]) i++;
   }
 
-  return ans3;
+  return ans;
 }
 
 function fourSum(nums: number[], target: number): number[][] {
   nums.sort((a, b) => a - b);
 
-  const ans4: number[][] = [];
-  for (let i = 0; i < nums.length - 3; ) {
-    const curRes = treeSum(nums, i + 1, target - nums[i]);
-
-    for (let j = 0; j < curRes.length; j++) {
-      ans4.push([nums[i], ...curRes[j]]);
-    }
-
-    i++;
-    while (nums[i] == nums[i - 1]) i++;
-  }
-
-  return ans4;
+  return getSum(nums, 0, target, 3);
 }
 // @lc code=end
 /**
@@ -85,4 +77,7 @@ function fourSum(nums: number[], target: number): number[][] {
  * 2. 三数之和再转两数之和即可， 返回给四数之和用
  * 3. 两数之和直接算，返回给三数之和用
  * 4. 去重：所有数之和过程中都记得去重
+ *
+ * #通解#
+ * 经过四数之和，我找到了n数之和的通解，写成了递归函数getSum。使用前对数组进行排序，调用getSum即可得到n数之和
  */
