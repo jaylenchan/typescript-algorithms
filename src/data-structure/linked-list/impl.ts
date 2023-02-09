e0eea897cfd271eaf5100debf7b0b27f2a34320d
jaylen <jaylen.work@hotmail.com>
2023-02-09 15:17:51 +0800

    feat(data-structure): 链表寻找中点相关信息的方法

--- src/data-structure/linked-list/impl.ts
+++ src/data-structure/linked-list/impl.ts
@@ -146,6 +146,138 @@ class LinkedList<E> implements ILinkedList<E> {
     }
   }
 
+  /**
+   * 返回链表的中点或者上中点（奇数中点 | 偶数上中点）
+   */
+  getMidOrUpMidNode(head: ListNode | null): ListNode | null {
+    /**
+     * 处理0 | 1 | 2 个节点的情况
+     * # head == null ： 链表不存在吗？
+     * # head.next == null ： 链表没法走一步吗？（暗示除了头结点，还有1个节点）
+     * # head.next.next == null ： 链表没法走两步吗？（暗示除了头结点，还有2个节点）
+     */
+    if (head == null || head.next == null || head.next.next == null) {
+      return head;
+    }
+
+    /**
+     * # 链表至少能在头节点开始往前走出两步（暗示逻辑走到这里， 至少3个节点）
+     * 从这里开始考虑，就可以让快指针fast大胆的走出两步
+     *
+     * @起始位置-定制
+     * @slow从头结点出发，先走一步（来到第2个节点）
+     * @fast从头节点出发，先走两步（来到第3个节点）
+     */
+    let slow = head.next;
+    let fast = head.next.next;
+
+    // 循环不变量：站在fast所在位置，能够不断往下走两步
+    while (fast.next && fast.next.next) {
+      slow = slow.next!;
+      fast = fast.next.next;
+    }
+
+    return slow;
+  }
+
+  /**
+   * 返回链表的中点或者上中点的前一个点（奇数中点 | 偶数上中点）
+   */
+  getfMidOrUpMidNodePrev(head: ListNode | null): ListNode | null {
+    /**
+     * 处理0 | 1 | 2 个节点的情况
+     * # head == null ： 链表不存在吗？
+     * # head.next == null ： 链表没法走一步吗？（暗示除了头结点，还有1个节点）
+     * # head.next.next == null ： 链表没法走两步吗？（暗示除了头结点，还有2个节点）
+     */
+    if (head == null || head.next == null || head.next.next == null) {
+      return null;
+    }
+
+    /**
+     * # 链表至少能在头节点开始往前走出两步（暗示逻辑走到这里， 至少3个节点）
+     * 从这里开始考虑，就可以让快指针fast大胆的走出两步
+     *
+     * @起始位置-定制
+     * @slow从头结点出发
+     * @fast从头节点出发，先走两步（来到第3个节点）
+     */
+    let slow = head;
+    let fast = head.next.next;
+
+    while (fast.next != null && fast.next.next != null) {
+      slow = slow.next!;
+      fast = fast.next.next;
+    }
+
+    return slow;
+  }
+
+  /**
+   * 返回链表的中点或者下中点（奇数中点 | 偶数下中点）
+   */
+  getMidOrDownMidNode(head: ListNode | null): ListNode | null {
+    /**
+     * 处理 0 | 1个节点的情况
+     * # head == null 链表不存在吗？
+     * # head.next == null 站在链表的头节点，能往下走一步吗？（来到第2个点）
+     */
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    /**
+     * # 链表至少能在头结点出发走出一步（暗示逻辑走到这里至少有2个点）
+     *
+     * @起始位置-定制
+     * @slow从头结点出发，先走一步（来到第2个节点）
+     * @fast从头节点出发，先走一步（来到第2个节点）
+     */
+    let slow = head.next;
+    let fast = head.next;
+
+    // 循环不变量：站在fast所在位置，能够不断往下走两步
+    while (fast.next && fast.next.next) {
+      slow = slow.next!;
+      fast = fast.next.next;
+    }
+
+    return slow;
+  }
+
+  /**
+   * 返回链表的中点或者下中点的上一个点（奇数中点 | 偶数下中点）
+   */
+  getfMidOrDownMidNodePrev(head: ListNode | null): ListNode | null {
+    /**
+     * 处理0 | 1 | 2 个节点的情况
+     * # head == null ： 链表不存在吗？
+     * # head.next == null ： 链表没法走一步吗？（暗示除了头结点，还有1个节点）
+     * # head.next.next == null ： 链表没法走两步吗？（暗示除了头结点，还有2个节点）
+     */
+    if (head == null || head.next == null) {
+      return null;
+    }
+
+    /**
+     * # 链表至少能在头节点开始往前走出两步（暗示逻辑走到这里， 至少3个节点）
+     * 从这里开始考虑，就可以让快指针fast大胆的走出两步
+     *
+     * @起始位置-定制
+     * @slow从头结点出发,先走一步（来到第2个节点）
+     * @fast从头节点出发，先走一步（来到第2个节点）
+     */
+    let slow = head;
+    let fast = head.next;
+
+    while (fast.next != null && fast.next.next != null) {
+      slow = slow.next!;
+      fast = fast.next.next;
+    }
+
+    return slow;
+  }
+
   public toString(): string {
     let res = '';
     let cur = this.dummyHead.next;
