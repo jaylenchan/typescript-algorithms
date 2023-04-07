10dbe5881bf6c4a93cbc75b24fb0c7d34cf011b5
Jaylenchan <jaylen.chan@hotmail.com>
2023-04-07 18:39:11 +0800

    feat(data-structure): linkedlist impl1

---
+++ src/data-structure/LinkedList/LinkedListNode.ts
@@ -0,0 +1,17 @@
+class LinkedListNode<V> {
+  static readonly None = new LinkedListNode<any>(null)
+
+  public value: V;
+  public next: LinkedListNode<V>
+
+  constructor(value: V) {
+    this.value = value
+    this.next = LinkedListNode.None
+  }
+
+  public toString(): string {
+    return `${this.value}`
+  }
+}
+
+export default LinkedListNode
--- src/data-structure/LinkedList/impl1.ts
+++ src/data-structure/LinkedList/impl1.ts
@@ -1,123 +1,193 @@
-export class Node<V> {
-  static readonly None = new Node<any>(undefined)
-
-  public value: V;
-  public next: Node<V>
-
-  constructor(value: V) {
-    this.value = value
-    this.next = Node.None
-  }
-}
+import LinkedListNode from './LinkedListNode';
 
 export class LinkedList<V> {
-  private _size: number = 0
-  public first: Node<V> = Node.None
+  private head: LinkedListNode<V> = LinkedListNode.None
+  private tail: LinkedListNode<V> = LinkedListNode.None
+  private size: number = 0
 
-  public get size(): number {
-    return this._size;
+  // 在链表头部插入新结点
+  public prepend(value: V): void {
+    const newNode = new LinkedListNode(value)
+
+    newNode.next = this.head
+    this.size++
   }
 
-  public isEmpty(): boolean {
-    return this.first == Node.None
-  }
+  // 在链表尾部插入新结点
+  public append(value: V): void {
+    const newNode = new LinkedListNode(value)
+    /**
+     * 情况1： tail是none的情况
+     *
+     * 由于只要head不为none，tail一定是不为none的。因为在只有1个结点的情况下，head跟tail指向的一定是同一个结点，这种情况下tail不可能为none。
+     * 而当且仅当head为none，整条链表为空的情况下，tail一定也为none。
+     * 总结：tail跟head在两种情况始终相同：链表结点个数为0，链表结点个数为1的情况。tail为none表示链表为空
+     */
 
-  insert(index: number, value: V): void {
-    if (index < 0 || index > this._size) {
-      // 异常情况特判
-      throw new Error("invalid index!")
+    if (this.tail == LinkedListNode.None) {
+      // 处理链表为空的情况
+      this.head = newNode
+      this.tail = newNode
+      this.tail.next = LinkedListNode.None
     } else {
-      const node = new Node(value)
+      // 处理链表不为空的情况
+      const oldTail = this.tail
 
-      if (this.isEmpty()) {
-        this.first = node
-        this.first.next = Node.None
-      } else {
-        let cur = 0;
-        let curNode = this.first
+      oldTail.next = newNode
+      newNode.next = LinkedListNode.None
+    }
 
-        while (cur != index) {
+    this.size++
+
+  }
+
+  // 普遍的插入新结点
+  public insert(value: V, index: number): void {
+    if (index < 0 || index > this.size) {
+      index = index < 0 ? 0 : this.size
+    }
+
+    switch (index) {
+      case (0): {
+        this.prepend(value)
+        break;
+      }
+
+      case (this.size - 1): {
+        this.append(value)
+        break;
+      }
+
+      default: {
+        // 除了在插进0号位，和插进结尾位，剩下的插入位置都可以通过找到它之前的位置，基于index之前的位置去做标准进行插入
+        let cur = 0
+        let curNode = this.head
+
+        while (cur != index - 1) {
+          cur++
           curNode = curNode.next
         }
 
-        const oldNext = curNode.next;
+        const newNode = new LinkedListNode(value)
 
-        curNode.next = node
-        node.next = oldNext
+        curNode.next = newNode
+
+        if (this.tail == curNode) {
+          this.tail = newNode
+        }
+
       }
-
-      this._size++
     }
   }
 
-  get(index: number): V {
-    if (index < 0 || index > this._size - 1) {
-      throw new Error("invalid index!")
+  // 删除节点
+  public delete(value: V): LinkedListNode<V> | null {
+    let curNode = this.head;
+
+    if (curNode.next == LinkedListNode.None) {
+      const deleteNode = curNode
+
+      curNode = LinkedListNode.None
+      this.tail = this.head
+
+      return deleteNode
     } else {
-      let cur = 0
-      let curNode = this.first
+      while (curNode != LinkedListNode.None) {
+        let nextNode = curNode.next
 
-      while (cur != index) {
-        curNode = curNode.next
-        cur++
-      }
+        if (nextNode.value == value) {
+          const deleteNode = nextNode
 
-      return curNode.value
-    }
-  }
+          curNode.next = deleteNode.next
+          nextNode = LinkedListNode.None
 
-  contains(value: V): boolean {
-    if (this.isEmpty()) {
-      throw new Error("list is empty!")
-    } else {
-      let curNode = this.first
-
-      while (curNode != Node.None) {
-        if (curNode.value == value) {
-          return true
+          return deleteNode
         }
 
         curNode = curNode.next
       }
+    }
 
-      return false;
+    return null
+  }
+
+  // 删除头结点
+  public deleteHead(): LinkedListNode<V> | null {
+    if (this.head == LinkedListNode.None) {
+      return null
+    } else {
+      let oldHead = this.head;
+      const deleteNode = oldHead
+
+      this.head = oldHead.next
+      oldHead = LinkedListNode.None
+
+      return deleteNode
     }
   }
 
-  set(index: number, value: V): void {
-    if (index < 0 || index > this.size - 1) {
-      throw new Error("invalid index!")
+  // 删除尾结点
+  public deleteTail(): LinkedListNode<V> | null {
+    if (this.tail == LinkedListNode.None) {
+      return null
     } else {
-      let cur = 0
-      let curNode = this.first
+      let curNode = this.head;
 
-      while (cur != index) {
+      while (curNode.next != this.tail) {
         curNode = curNode.next
-        cur++
       }
 
-      curNode.value = value
+      let oldTail = curNode.next;
+      const deleteNode = oldTail
+
+      curNode.next = LinkedListNode.None
+      oldTail.next = LinkedListNode.None
+
+      return deleteNode
     }
   }
 
-  remove(index: number) {
-    if (index < 0 || index > this.size - 1) {
-      throw new Error("invalid index!")
-    } else {
-      let cur = 0
-      let curNode = this.first
+  public fromArray(values: V[]): void {
+    for (let i = 0; i < values.length; i++) {
+      const value = values[i]
 
-      while (cur != index - 1) {
-        curNode = curNode.next
-        cur++
-      }
-
-      let deleteNode = curNode.next
-      const newNextNode = deleteNode.next
-
-      curNode.next = newNextNode
-      deleteNode = Node.None
+      this.append(value)
     }
   }
 
+  public toArray(): LinkedListNode<V>[] {
+    const nodes: LinkedListNode<V>[] = []
+
+    let curNode = this.head;
+
+    while (curNode != LinkedListNode.None) {
+      nodes.push(curNode)
+
+      curNode = curNode.next
+    }
+
+    return nodes
+  }
+
+  public reverse(): void {
+    let prevNode = LinkedListNode.None
+    let curNode = this.head
+    let nextNode = LinkedListNode.None
+
+    while (curNode) {
+      nextNode = curNode.next
+      curNode.next = prevNode
+      prevNode = curNode
+      curNode = nextNode
+    }
+
+    this.head = prevNode
+    this.tail = this.head
+  }
+
+  public toString(): string {
+    return JSON.stringify(this.toArray().map((node) => {
+      return node.toString()
+    }))
+  }
 }
