31c081dc5aebe1c1a5031b0113e82541842e8e85
Jaylenchan <jaylen.chan@hotmail.com>
2023-04-07 23:03:39 +0800

    chore(structures): fix Heap eslint

--- structures/Heap/MaxHeap-impl.ts
+++ structures/Heap/MaxHeap-impl.ts
@@ -1,45 +1,45 @@
-import DArray from '../array/impl'
+import DArray from '../Array/impl'
 
 import type IHeap from './interface'
 
 
 class MaxHeap<E> implements IHeap<E> {
 
-  private data: DArray<E>
+  private _data: DArray<E>
 
   // 堆的构造函数接收用户传递的容量，不传递的话由动态数组自身默认的容量决定
   constructor(capacity?: number) {
-    this.data = new DArray(capacity)
+    this._data = new DArray(capacity)
   }
 
-  public size() {
-    return this.data.getSize()
+  public size(): number {
+    return this._data.getSize()
   }
 
   public isEmpty(): boolean {
-    return this.data.isEmpty()
+    return this._data.isEmpty()
   }
 
   public add(e: E): void {
     // 在数组最后加入元素
-    this.data.addLast(e)
+    this._data.addLast(e)
     // 使用siftUp进行堆元素调整，重新符合堆的结构定义
-    this._siftUp(this.data.getSize() - 1)
+    this._siftUp(this._data.getSize() - 1)
   }
 
   public findMax(): E {
-    if (this.data.isEmpty()) {
+    if (this._data.isEmpty()) {
       throw new Error('Can not findMax when heap is empty.')
     }
 
-    return this.data.get(0)
+    return this._data.get(0)
   }
 
   public extractMax(): E {
     const result = this.findMax()
 
-    this.data.swap(0, this.data.getSize() - 1)
-    this.data.removeLast()
+    this._data.swap(0, this._data.getSize() - 1)
+    this._data.removeLast()
     this._siftDown(0)
 
     return result
@@ -47,18 +47,18 @@ class MaxHeap<E> implements IHeap<E> {
 
   public replace(e: E): E {
     const result = this.findMax()
-    this.data.set(0, e)
+    this._data.set(0, e)
     this._siftDown(0)
 
     return result
   }
 
-  public heapify(arr: E[]) {
-    const data = new DArray<E>(arr.length)
+  public heapify(arr: E[]): void {
+    const _data = new DArray<E>(arr.length)
     for (let i = 0; i < arr.length; i++) {
-      data.add(i, arr[i])
+      _data.add(i, arr[i])
     }
-    this.data = data
+    this._data = _data
 
     for (let i = this._parent(arr.length - 1); i >= 0; i--) {
       this._siftDown(i)
@@ -66,7 +66,7 @@ class MaxHeap<E> implements IHeap<E> {
   }
 
   /** 返回完全二叉树的数组表示当中，一个索引所表示的元素的父亲节点的索引（使用堆的计算方式1，从0开始放节点） */
-  private _parent(index: number) {
+  private _parent(index: number): number {
     if (index == 0) {
       // 索引0无父亲节点
       throw new Error("index-0 doesn't have parent.")
@@ -76,20 +76,20 @@ class MaxHeap<E> implements IHeap<E> {
   }
 
   /** 返回完全二叉树的数组表示当中，一个索引所表示的元素的左孩子节点的索引（使用堆的计算方式1，从0开始放节点） */
-  private _leftChild(index: number) {
+  private _leftChild(index: number): number {
     return index * 2 + 1
   }
 
   /** 返回完全二叉树的数组表示当中，一个索引所表示的元素的右孩子节点的索引（使用堆的计算方式1，从0开始放节点） */
-  private _rightChild(index: number) {
+  private _rightChild(index: number): number {
     return index * 2 + 2
   }
 
   /** 添加堆元素时使用的额外操作 */
   /** 索引k表示从哪个地方开始往上浮动元素 */
   private _siftUp(k: number): void {
-    while (k > 0 && this.data.get(this._parent(k)) < this.data.get(k)) {
-      this.data.swap(k, this._parent(k))
+    while (k > 0 && this._data.get(this._parent(k)) < this._data.get(k)) {
+      this._data.swap(k, this._parent(k))
       k = this._parent(k)
     }
   }
@@ -99,12 +99,12 @@ class MaxHeap<E> implements IHeap<E> {
   private _siftDown(k: number): void {
     // 判断k的左孩子是否存在的依据：左孩子的索引要小于数组的大小
     // 能够进入循环就说明左孩子一定存在
-    while (this._leftChild(k) < this.data.getSize()) {
+    while (this._leftChild(k) < this._data.getSize()) {
       let j = this._leftChild(k)
       // 判断右孩子是否存在 && 判断右孩子是否比左孩子数值要大
-      if (j + 1 < this.data.getSize()) {
-        // 经过以上this.data[j]就是leftChild和rightChild当中的最大值
-        if (this.data.get(j + 1) > this.data.get(j)) {
+      if (j + 1 < this._data.getSize()) {
+        // 经过以上this._data[j]就是leftChild和rightChild当中的最大值
+        if (this._data.get(j + 1) > this._data.get(j)) {
           j = this._rightChild(k)
         }
       }
@@ -112,13 +112,13 @@ class MaxHeap<E> implements IHeap<E> {
       // 判断k位置元素跟它的左右孩子中最大的那个孩子的数值，看是否大于左右孩子中最大的那个孩子的数值
       // 如果是的话，直接退出循环
       // 这里的操作主要是为了判断是否继续交换+更新k，为了满足堆定义后退出循环做保证
-      if (this.data.get(k) > this.data.get(j)) {
+      if (this._data.get(k) > this._data.get(j)) {
         break
       }
 
       // 下沉交换+更新k的主要逻辑是这里两个操作
       // 如果以上的退出循环条件不中，执行交换操作， 交换k位置节点和左右孩子中最大的孩子
-      this.data.swap(k, j)
+      this._data.swap(k, j)
       // 交换完毕后，更新k位置
       k = j
     }
